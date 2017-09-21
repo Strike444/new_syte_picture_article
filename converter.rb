84ddc5347b1c3f12a08625b11cc44b488c472b51
Andrey Shimanovich <strikebgu84@mail.ru>
2017-09-21 17:20:28 +0300

    add dir contents

--- converter.rb
+++ converter.rb
@@ -1,35 +1,89 @@
-require 'optparse'
+# Program for converting jpg and getting text from word formats
 
-options = {:path => nil}
+VERSION = '0.0.1'
 
-parser = OptionParser.new do|opts|
-  opts.banner = "Usage: ruby converter.rb [options]"
-  opts.on('-p', '--path path', 'Path') do |path|
-    options[:path] = path;
+class Parse_command_line 
+
+  require 'optparse'
+
+  def initialize() 
+    @path = ''
+    @version = VERSION
   end
 
-  opts.on('-h', '--help', 'Displays Help') do
-    puts opts
-    exit
+  def path
+    @path
+  end
+
+  def parse_cl()
+    parser = OptionParser.new do|opts|
+      opts.banner = "Usage: ruby converter.rb [options]"
+      opts.on('-p', '--path path', 'Path') do |path|
+        @path = path;
+      end
+      opts.on('-h', '--help', 'Displays Help') do
+        puts opts
+        exit
+      end
+      opts.on('-v', '--version', 'Displays Version') do
+        puts "Current version is " + @version
+        exit
+      end
+    end
+
+    parser.parse!
+
+    if @path == nil
+      print 'Enter path to dir with files: '
+      @path = gets.chomp
+    end
+
+    loop do
+      if File.directory?(@path)
+        break
+      else
+        puts "Path is wrong or not exist, plis enter valid path:"
+        @path = gets.chomp
+      end
+    end
   end
 end
 
-parser.parse!
+class Dir_contents
 
-if options[:path] == nil
-  print 'Enter path to dir with files: '
-  options[:path] = gets.chomp
-end
+  def initialize(path)
+    @path = path
+  end
 
-loop do
-  if File.directory?(options[:path])
-    break
-  else
-    puts "Path is wrong or not exist, plis enter valid path:"
-    options[:path] = gets.chomp
+  def path
+    @path
+  end
+
+  def dir_contents()
+    dir_c = Array.new
+    dir = Dir.open("#{@path}")
+    dir.each {|e| dir_c << e}
+    return dir_c
   end
 end
 
-puts options[:path]
-    
+pc = Parse_command_line.new()
+pc.parse_cl
+puts "Путь к каталогу: #{pc.path}"
+dr = Dir_contents.new(pc.path)
+puts dr.dir_contents()
 
+    # @i = 0
+    # dir.each do |file| 
+    #   if File.file?(@path + file) then
+    #     puts file
+    #     old_name = File.basename(file,'.*')
+    #     # if old_name.include?(search)
+    #     # File.rename( file, new_path )
+    #     # new_name = File.basename(file).gsub(target,replace)
+    #     # puts "Renamed #{file} to #{new_name}" if $DEBUG
+    #     puts old_name
+    #     new_name = file.gsub(File.basename(file,'.*'),@i)
+    #     @i++
+    #     puts new_name
+    #   end
